ef1ca0f99bbafd4a8129497949005a9d6f895484
Dan Gottlieb <dan@gotdan.org>
2020-04-01 10:22:47 -0400

    typo

--- src/components/Wizard/index.js
+++ src/components/Wizard/index.js
@@ -34,7 +34,7 @@ export default() => {
 		<Row><Col>
 			<h2 style={{color: "#3182CE", padding: "1em 0", textAlign: "center"}}>
 				<FontAwesomeIcon icon={faMedkit} alt="Procure logo" className="mr-2" />
-				<span>{appName} || "Procure"</span>
+				<span>{appName || "Procure"}</span>
 			</h2>
 		</Col></Row>
 		<Row><Col md={{size:10, offset: 1}} lg={{size:8, offset: 2}}>
